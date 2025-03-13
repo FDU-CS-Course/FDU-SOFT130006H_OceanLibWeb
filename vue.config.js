74a6ef8a1f314166bf96a88e368f2594a743e0db
cms42 <c@cms42.top>
2025-03-13 16:21:47 +0800

    chore: config vue to use port 5432, avoiding collpase with backend

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,25 @@
 const CompressionPlugin = require('compression-webpack-plugin')
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  css: {},
-  configureWebpack: {
-    externals: {
-      // 'vue': 'Vue',
-      // 'axios': 'axios'
+    transpileDependencies: [
+        'vuetify'
+    ],
+    css: {},
+    configureWebpack: {
+        externals: {
+            // 'vue': 'Vue',
+            // 'axios': 'axios'
+        },
+        plugins: [new CompressionPlugin({
+            test: /\.(js|css)(\?.*)?$/i, // 需要压缩的文件正则
+            threshold: 10240, // 对超过10k的数据进行压缩
+            deleteOriginalAssets: false // 是否删除原文件
+        })]
     },
-    plugins: [new CompressionPlugin({
-      test: /\.(js|css)(\?.*)?$/i, // 需要压缩的文件正则
-      threshold: 10240, // 对超过10k的数据进行压缩
-      deleteOriginalAssets: false // 是否删除原文件
-    })]
-  },
-  pluginOptions: {
-    vuetify: {}
-  }
+    pluginOptions: {
+        vuetify: {}
+    },
+    devServer: {
+        port: 5432,
+        open: true,
+    }
 }
