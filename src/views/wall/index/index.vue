6bc721b2035291cb9a28b5ffd434713c104fc2b8
AInfinity-LilacDream <1977741520@qq.com>
2025-05-17 22:00:36 +0800

    feat: refactor wall page

--- src/views/wall/index/index.vue
+++ src/views/wall/index/index.vue
@@ -24,6 +24,7 @@
     }
     &__type {
       font-size: 16px;
+      padding-left: 5px;
     }
     &__content {
       font-size: 14px;
@@ -34,15 +35,18 @@
     }
   }
 }
+
+.title {
+  margin-bottom: 20px;
+}
 </style>
 <template>
-  <div class="wall">
-    <div class="text-h5" style="margin:20px 0px">互助集市 <v-icon color="rgb(var(--v-theme-primary))">mdi-sticker</v-icon>
-    </div>
+  <div class="title">
+    <div class="text-h4" style="margin:30px 40px">互助集市 <v-icon color="orange">mdi-sticker</v-icon></div>
     <van-sticky :offset-top="0">
       <div class="wall__search">
-        <v-toolbar theme="dark" color="rgb(var(--v-theme-primary))">
-          <v-text-field hide-details prepend-icon="mdi-magnify" single-line></v-text-field>
+        <v-toolbar color="black">
+          <v-text-field hide-details prepend-icon="mdi-magnify" single-line style="padding-left: 20px;"></v-text-field>
           <v-btn icon>
             <v-icon>mdi-filter</v-icon>
           </v-btn>
@@ -52,52 +56,37 @@
         </v-toolbar>
       </div>
     </van-sticky>
+  </div>
 
+  <div class="wall">
     <div>
       <div v-for="(item,index) in wallInfo" :key="index">
 
-        <v-card class="mx-auto mb-4" v-if="item.isAD" theme="dark" max-width="400">
+        <v-card class="mx-auto mb-4" v-if="item.isAD" theme="dark">
           <v-parallax height="150" src="@/images/ad-2.png"></v-parallax>
         </v-card>
 
-        <v-card class="mx-auto mb-4" :color="item.backgroundColor" theme="dark" max-width="400" v-else>
-          <v-card-title>
-            <v-icon left>
-              {{item.noticeIcon?item.noticeIcon:getIcon(item.noticeType)}}
-            </v-icon>
-            <span class="wall__card__type">{{item.noticeTypeName?item.noticeTypeName:getTypeName(item.noticeType)}}</span>
+        <v-card class="mx-auto mb-4" style="border-radius: 20px; background-color: white;" v-else>
+          <v-card-title style="padding: 30px 30px;">
+            <span class="wall__card__type text-h4">{{ item.tag }}</span>
           </v-card-title>
 
           <v-card-text class="wall__card__content">
-            {{item.noticeContent}}
+            {{ item.content }}
           </v-card-text>
 
           <v-card-actions>
-            <v-list-item class="grow">
-              <v-avatar color="grey darken-3" size="28" v-if="!item.isAnonymous">
-                <v-img class="elevation-6" :src="item.buildUserEntity.avatar" alt="Avatar" v-if="item.buildUserEntity.avatar != null" />
-                <span v-else>{{ item.buildUserEntity.nickname.substring(0, 1) }}</span>
-              </v-avatar>
-              <v-avatar color="grey darken-3" size="28" v-else>
-                <v-img class="elevation-6"
-                  src="https://avataaars.io/?avatarStyle=Transparent&topType=Hat&accessoriesType=Round&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Serious&skinColor=Light"
-                  alt="Avatar" />
-              </v-avatar>
-<!--              <v-list-item-content>-->
-              <div class="text-body-2">{{item.isAnonymous?"匿名纸条":item.buildUserEntity.nickname}}</div>
-<!--              </v-list-item-content>-->
-              <v-row align="center" justify="end">
-                <v-icon class="mr-1">
-                  mdi-comment-eye
-                </v-icon>
-                <span class="text-caption mr-2">{{item.focusNum}}</span>
+            <div class="d-flex align-center justify-space-between w-100">
+              <div class="text-h6" style="padding-left: 30px;">{{ item.isAnon ? "匿名纸条" : item.buildUsername }}</div>
+
+              <div class="d-flex align-center" style="padding-right: 30px;">
+                <v-icon small class="mr-1">mdi-comment-eye</v-icon>
+                <span class="text-caption mr-2">{{ item.readNum }}</span>
                 <span class="mr-1">·</span>
-                <v-icon class="mr-1">
-                  mdi-comment
-                </v-icon>
-                <span class="text-caption">{{item.commentNum}}</span>
-              </v-row>
-            </v-list-item>
+                <v-icon small class="mr-1">mdi-comment</v-icon>
+                <span class="text-caption">{{ item.commentNum }}</span>
+              </div>
+            </div>
           </v-card-actions>
         </v-card>
       </div>
@@ -119,160 +108,122 @@
       </v-bottom-navigation>
     </div>
     <div style="position: fixed; bottom: 60px; right: 10px;">
-      <v-fab
-        :color="fab ? '' : 'primary'"
-        location="bottom right"
-        icon
-        size="small"
-      >
-        <v-icon>{{ fab ? 'mdi-close' : 'mdi-fountain-pen-tip' }}</v-icon>
-        
-        <v-speed-dial
+      <v-btn
           v-model="fab"
-          direction="top"
-          transition="slide-y-reverse-transition"
-          activator="parent"
-        >
-          <v-btn
-            color="green"
-            icon
-            size="small"
-          >
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
+          :color="fab ? 'white' : 'orange'"
+          location="bottom end"
+          icon
+          size="small"
+          fab
+      >
+        <v-icon :icon="fab ? 'mdi-close' : 'mdi-fountain-pen-tip'"></v-icon>
 
-          <v-btn
-            color="red"
-            icon
-            size="small"
-          >
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </v-speed-dial>
-      </v-fab>
+        <v-menu
+            v-model="fab"
+            activator="parent"
+            location="top"
+            transition="slide-y-reverse-transition"
+        >
+              <v-btn
+                  style="background-color:green; margin-bottom: 20px;"
+                  color="white"
+                  variant="text"
+                  icon="mdi-pencil"
+                  size="small"
+                  fab
+              ></v-btn>
+              <v-btn
+                  style="background-color:red; margin-bottom: 20px;"
+                  color="white"
+                  variant="text"
+                  icon="mdi-delete"
+                  size="small"
+                  fab
+              ></v-btn>
+        </v-menu>
+      </v-btn>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      fab: true,
-      navigation: 1,
-      wallInfo: [
-        {
-          backgroundColor: '#3C5C8B',
-          noticeType: '1',
-          noticeContent: '求计算机大佬帮孩子看看C++，可以请奶茶，留个QQ：1257477940',
-          buildUserEntity: {
-            nickname: 'Alpha测试',
-            avatar:
-              'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
-          },
-          isAnonymous: true,
-          focusNum: '450',
-          commentNum: '25',
-        },
-        {
-          backgroundColor: '#4D84B6',
-          noticeType: '2',
-          noticeContent: '有无计算机院费老师的离散数学ppt可以分享一下呀，孩子马上要考试了，急需这个东西',
-          buildUserEntity: {
-            nickname: '北理派大星',
-            avatar:
-              'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
-          },
-          isAnonymous: false,
-          focusNum: '78',
-          commentNum: '2',
-        },
-        {
-          isAD: true,
-        },
-        {
-          backgroundColor: '#83ADCE',
-          noticeType: '3',
-          noticeContent: '捞一下昨天在二食堂看见的一个穿红色衣服的小哥哥，大概180，我把汤洒他身上了',
-          buildUserEntity: {
-            nickname: 'Alpha测试3',
-            avatar:
-              'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription01&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
-          },
-          isAnonymous: false,
-          focusNum: '5',
-          commentNum: '0',
-        },
-        {
-          backgroundColor: '#93A4B9',
-          noticeType: '4',
-          noticeContent: '孩子把新买的平板扔在新校区A座305了，捡到请联系13305007890，重谢',
-          buildUserEntity: {
-            nickname: 'Alpha测试4',
-            avatar:
-              'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription03&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
-          },
-          isAnonymous: false,
-          focusNum: '120',
-          commentNum: '1',
-        },
-        {
-          backgroundColor: '#93A4B9',
-          noticeType: '4',
-          noticeContent: '测试',
-          buildUserEntity: {
-            nickname: 'Alpha测试4',
-            avatar:
-              'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription03&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
-          },
-          isAnonymous: false,
-          focusNum: '120',
-          commentNum: '1',
-        },
-        {
-          backgroundColor: '#93A4B9',
-          noticeType: '4',
-          noticeContent: '测试',
-          buildUserEntity: {
-            nickname: 'Alpha测试4',
-            avatar:
-              'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription03&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
-          },
-          isAnonymous: false,
-          focusNum: '120',
-          commentNum: '1',
-        },
-      ],
-    };
+<script setup>
+import { ref } from 'vue';
+
+const fab = ref(true);
+const navigation = ref(1);
+
+const wallInfo = [
+  {
+    noteID: '1',
+    tag: "问题",
+    content: "求计算机大佬帮孩子看看C++，可以请奶茶，留个QQ：1257477940",
+    likeNum: 0,
+    commentNum: 25,
+    readNum: 35,
+    refreshDate: '',
+    buildDate: '',
+    buildUsername: "Alpha测试",
+    isDeleted: false,
+    isAnon: false,
+    isAllowComment: true,
   },
-  mounted() {},
-  methods: {
-    getIcon(noticeType) {
-      switch (noticeType) {
-        case '1':
-          return 'mdi-help-circle';
-        case '2':
-          return 'mdi-note-search';
-        case '3':
-          return 'mdi-account-search';
-        case '4':
-          return 'mdi-map-marker-radius';
-      }
-    },
-    getTypeName(noticeType) {
-      switch (noticeType) {
-        case '1':
-          return '问题';
-        case '2':
-          return '文档帮帮';
-        case '3':
-          return '捞人';
-        case '4':
-          return '寻物';
-      }
-    },
+  {
+    noteID: '1',
+    tag: "问题",
+    content: "求计算机大佬帮孩子看看C++，可以请奶茶，留个QQ：1257477940",
+    likeNum: 0,
+    commentNum: 25,
+    readNum: 35,
+    refreshDate: '',
+    buildDate: '',
+    buildUsername: "Alpha测试",
+    isDeleted: false,
+    isAnon: false,
+    isAllowComment: true,
   },
-};
+  {
+    noteID: '1',
+    tag: "问题",
+    content: "求计算机大佬帮孩子看看C++，可以请奶茶，留个QQ：1257477940",
+    likeNum: 0,
+    commentNum: 25,
+    readNum: 35,
+    refreshDate: '',
+    buildDate: '',
+    buildUsername: "Alpha测试",
+    isDeleted: false,
+    isAnon: false,
+    isAllowComment: true,
+  },
+  {
+    noteID: '1',
+    tag: "问题",
+    content: "求计算机大佬帮孩子看看C++，可以请奶茶，留个QQ：1257477940",
+    likeNum: 0,
+    commentNum: 25,
+    readNum: 35,
+    refreshDate: '',
+    buildDate: '',
+    buildUsername: "Alpha测试",
+    isDeleted: false,
+    isAnon: false,
+    isAllowComment: true,
+  },
+  {
+    noteID: '1',
+    tag: "问题",
+    content: "求计算机大佬帮孩子看看C++，可以请奶茶，留个QQ：1257477940",
+    likeNum: 0,
+    commentNum: 25,
+    readNum: 35,
+    refreshDate: '',
+    buildDate: '',
+    buildUsername: "Alpha测试",
+    isDeleted: false,
+    isAnon: false,
+    isAllowComment: true,
+  }
+];
 </script>
 
 <style scoped>
